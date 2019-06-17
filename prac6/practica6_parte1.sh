a4ee8fcf14025a733778ae76066a92e04c73ff17
zoroCR <guillecruzrojas@hotmail.com>
2019-06-17 12:43:11 +0200

    kdsfl

--- prac6/practica6_parte1.sh
+++ prac6/practica6_parte1.sh
@@ -3,13 +3,8 @@
 #Guillermo Cruz Rojas	682433
 #Miguel Gomez Lahera	741302
 
-if [ $# != 1 ]
-then
-	echo "Numero de parametros incorrectos"
-	exit 1
-fi
-
-ip=$1
+ruta="~/.ssh/id_as_ed25519"
+ip=192.168.56.2
 
 ping -c2 $ip >/dev/null 2>&1
 
@@ -19,17 +14,23 @@ then
 	exit 2
 fi
 
+echo "uptime0 > " | logger -p local0.info
 uptime | cut -d "," -f2,3,4,5 | logger -p local0.info
-free -h | awk '{print $1 "\t" $2 "\t" $3 "\t" $4}' | logger -p local0.info
-df -h | awk '{print $1 "\t" $2 "\t" $3}' | logger -p local0.info
+echo "free > " | logger -p local0.info
+free -h | awk '{print $1 "\t" $2 " " $3 " " $4}' | logger -p local0.info
+echo "df > " | logger -p local0.info
+df -h | awk '{print $1 " " $2 " " $3}' | logger -p local0.info
 netstat | awk 'BEGIN { x=0 } /CONNECTED/ {x=x+1} END { print "Conexiones: ", x } END {print "Puertos abiertos: " NR-4}' | logger -p local0.info
-ps -e --user as | awk 'END {print "Procesos:\t" NR-1}' | logger -p local0.info
+ps -e --user "$USER" | awk 'END {print "Procesos: " NR-1}' | logger -p local0.info
 
 #Con esto hacemos logger de la segunda maquina desde ssh
-ssh as@$ip uptime | cut -d "," -f2,3,4,5 | logger -p local0.info
-ssh as@$ip free -h | awk '{print $1 "\t" $2 "\t" $3 "\t" $4}' | logger -p local0.info
-ssh as@$ip df -h | awk '{print $1 "\t" $2 "\t" $3}' | logger -p local0.info
-ssh as@$ip netstat | awk 'BEGIN { x=0 } /CONNECTED/ {x=x+1} END { print "Conexiones: ", x } END {print "Puertos abiertos: " NR-4}' | logger -p local0.info
-ssh as@$ip ps -e --user as | awk 'END {print "Procesos:\t" NR-1}' | logger -p local0.info
+echo "uptime0 maquina $ip > " | logger -p local0.info
+ssh -n -i "$ruta" as@$ip uptime | cut -d "," -f2,3,4,5 | logger -p local0.info
+echo "free maquina $ip > " | logger -p local0.info
+ssh -n -i "$ruta" as@$ip free -h | awk '{print $1 " " $2 " " $3 " " $4}' | logger -p local0.info
+echo "df maquina $ip > " | logger -p local0.info
+ssh -n -i "$ruta" as@$ip df -h | awk '{print $1 " " $2 " " $3}' | logger -p local0.info
+ssh -n -i "$ruta" as@$ip netstat | awk 'BEGIN { x=0 } /CONNECTED/ {x=x+1} END { print "Conexiones de 192.168.56.2: ", x } END {print "Puertos abiertos de 192.168.56.2: " NR-4}' | logger -p local0.info
+ssh -n -i "$ruta" as@$ip ps -e --user as | awk 'END {print "Procesos de 192.168.56.2: " NR-1}' | logger -p local0.info
 
 #en uptime, loadaverage saca 3 numeros y son: load average ultimo minuto, el de los ultimos 5 minutos, y el de los ultimos 15 minutos
